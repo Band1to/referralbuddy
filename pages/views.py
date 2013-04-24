d10fd89b3351554a9750ea20e3c76f0735c3c0bc
Mohamed Mahmoud <mohamed.hammamy@webiken.net>
2013-04-24 03:02:51 -0400

    forms added to home page

--- pages/views.py
+++ pages/views.py
@@ -3,9 +3,11 @@ from django.http import HttpResponse
 from django.template import RequestContext
 
 from pages.models import Page, PageContent
+from registration.forms import RegistrationForm
 
 
 def home(request, template='index.html'):
+    form = RegistrationForm()
     try:
         content = PageContent.objects.filter(page__page_name='home_page', is_active=True).latest('page__pub_date')
     except:
@@ -14,7 +16,8 @@ def home(request, template='index.html'):
     return shortcuts.render_to_response(template,
             dict(
                 title = content.page.title if content else "Welcome", 
-                content = content
+                content = content,
+                form=form
             ),
             context_instance=RequestContext(request))
 
