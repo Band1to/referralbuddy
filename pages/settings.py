8d77375a91a8fa8382077fff63b7412513c99054
Sam Mohamed <sam@webiken.net>
2013-07-25 14:29:52 -0700

    added associate by email to the social auth pipeline

--- pages/settings.py
+++ pages/settings.py
@@ -130,6 +130,16 @@ AUTHENTICATION_BACKENDS = (
     'django.contrib.auth.backends.ModelBackend',
 )
 
+SOCIAL_AUTH_PIPELINE = (
+    'social_auth.backends.pipeline.social.social_auth_user',
+    'social_auth.backends.pipeline.associate.associate_by_email',
+    'social_auth.backends.pipeline.user.get_username',
+    'social_auth.backends.pipeline.user.create_user',
+    'social_auth.backends.pipeline.social.associate_user',
+    'social_auth.backends.pipeline.social.load_extra_data',
+    'social_auth.backends.pipeline.user.update_user_details'
+)
+
 INSTALLED_APPS = (
     ###DJANGO-APPS
     'django.contrib.auth',
