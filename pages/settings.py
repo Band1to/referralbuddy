23c35ef96bd3f1b0c2b65da5bc186f3ab8bd559b
Sam Mohamed <sam@webiken.net>
2013-07-26 15:14:15 -0700

    updated timzone to Australia/Sydney

--- pages/settings.py
+++ pages/settings.py
@@ -18,7 +18,7 @@ MANAGERS = ADMINS
 # http://en.wikipedia.org/wiki/List_of_tz_zones_by_name
 # although not all choices may be available on all operating systems.
 # In a Windows environment this must be set to your system time zone.
-TIME_ZONE = 'America/Chicago'
+TIME_ZONE = 'Australia/Sydney'
 
 # Language code for this installation. All choices can be found here:
 # http://www.i18nguy.com/unicode/language-identifiers.html
@@ -84,6 +84,7 @@ MIDDLEWARE_CLASSES = (
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
+    'pagination.middleware.PaginationMiddleware',
     # Uncomment the next line for simple clickjacking protection:
     # 'django.middleware.clickjacking.XFrameOptionsMiddleware',
 )
@@ -156,8 +157,8 @@ INSTALLED_APPS = (
     'kombu.transport.django',
     'paypal.standard.ipn',
     'tinymce',
-    'postman',
     'pagination',
+    'postman',
     'south',
     ###LOCAL APPS
     'pages',
